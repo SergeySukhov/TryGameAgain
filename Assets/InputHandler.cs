dead79b772b4351b45834beecf1c5d46ab2e8fbe
Александр Ржевский <arjevskiy@dev.local>
2019-12-06 12:26:47 +0300

    initial commit

---
+++ Assets/InputHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+
+    public Camera camera;
+    public List<GameObject> characterAssets = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Plane hPlane = new Plane(Vector3.up, Vector3.zero);
+            float distance = 0;
+            if (hPlane.Raycast(ray, out distance))
+            {
+                GameObject go2 = tileBehaviour.getClosestTile(ray.GetPoint(distance));
+                if (go2 != null)
+                {
+                    GameObject go = Instantiate(characterAssets[(int)Random.Range(0f, characterAssets.Count - 0.1f)], new Vector3(0, 0, 0), Quaternion.identity);
+                    go.transform.position = go2.transform.position;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/tileBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tileBehaviour : MonoBehaviour
+{
+    public static List<GameObject> tiles = new List<GameObject>();
+
+    public List<tileBehaviour> Neighbours = new List<tileBehaviour>(6);
+
+    public static GameObject getClosestTile(Vector3 point)
+    {
+        GameObject go = null;
+        float min = float.PositiveInfinity;
+        foreach(var tile in tiles)
+        {
+            var tmp = (tile.transform.position - point).sqrMagnitude;
+            if (tmp < min)
+            {
+                go = tile;
+                min = tmp;
+            }
+        }
+        return go;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        tiles.Add(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
